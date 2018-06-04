89d18ea717a4a33bb10af854a9f675bd80d85080
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2018-06-04 11:10:54 +0700

    web form validation in Server (not Front end)

---
+++ src/main/java/com/example/demo/SpringbootFormValidationApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://o7planning.org/en/11655/create-a-user-registration-application-with-spring-boot-spring-form-validation
+ *
+ */
+@SpringBootApplication
+public class SpringbootFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFormValidationApplication.class, args);
+	}
+}
